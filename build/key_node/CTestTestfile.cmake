a411914fdd4ed94ae488cc5e3666d1cb3403eba1
Youngdong-Clark-Son <clark.y.d.son@gmail.com>
2016-12-07 14:45:02 +0900

    File locations changed

--- build/key_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youngdong/ROS/keyboard_teleop/src/key_node
-# Build directory: /home/youngdong/ROS/keyboard_teleop/build/key_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/keyboard_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youngdong/ROS/keyboard_teleop/src/keyboard_teleop
-# Build directory: /home/youngdong/ROS/keyboard_teleop/build/keyboard_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
